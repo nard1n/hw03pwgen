6c1273e6fcd9731f41280d3cc8289a6f15ea6de6
Nardin Lachowski <nardin@Nardins-MacBook-Air.local>
2020-11-11 20:44:06 -0500

    added arrays for the various password criteria

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+// Password generator will create a password that has 8-128 characters based on criteria specified by the user through a series of prompts
+
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate").addEventListener('click', promptMe);
+  function promptMe (){
+  var userCriteria = prompt ("Want lower case letters?");
+  alert(userCriteria);
+}
+
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword());
+
+// Declarations
+
+// Password requirements - arrays
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +28,4 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
