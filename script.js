73f50e683f66897118702c294fd89ac5e9d953a6
Nardin Lachowski <nardin@live.com>
2020-11-12 22:07:34 -0500

    added arrays for password requirements

--- script.js
+++ script.js
@@ -2,22 +2,27 @@
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate").addEventListener('click', promptMe);
-  function promptMe (){
-  var userCriteria = prompt ("Want lower case letters?");
-  alert(userCriteria);
-}
 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
-
-// Declarations
-
-// Password requirements - arrays
+// Password requirements arrays
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+
+// Variables for checking criteria
+var confirmLength = "";
+var confirmLowerCase = "";
+
+// Prompts
+function promptMe (){
+  var userCriteria = prompt ("Want lower case letters?");
+  alert(userCriteria);
+}
+
+// logic and loops if wrong for each criteria/confirm popup - true if OK is pressed
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -27,5 +32,8 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
