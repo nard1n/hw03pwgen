1fb1dc009c9616a9d2919a9ba8cfb60d5bcdbf30
Nardin Lachowski <nardin@live.com>
2020-11-14 13:01:44 -0500

    corrected the password generator bug which prevented the password to respect new criteria after the first time the button is pressed

--- script.js
+++ script.js
@@ -4,8 +4,7 @@
 var generateBtn = document.querySelector("#generate");
 
 
-// Password requirements in arrays
-var passwordOutput = [];
+// List of available values for password generator to choose from listed in arrays
 var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
@@ -14,6 +13,8 @@ var specialChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~
 
 // Creating Function for generating the password
 function generatePassword () {
+  var passwordOutput = [];
+
   var confirmLength = (prompt("How many characters would you like the password to be? \n(between 8-128)"));
 
   //Loop if the number is outside 8 and 128
@@ -26,6 +27,7 @@ function generatePassword () {
   alert(`Your password will consist of ${confirmLength} characters`);
 
 // logic to determine if ..and loops if wrong for each criteria/confirm popup - true if OK is pressed
+
 var confirmLowerCase = confirm("Click OK if you'd like to include a lowercase letter");
 var confirmUpperCase = confirm ("Click Ok if you'd like to include an uppercase letter");
 var confirmNumber = confirm ("Click Ok if you'd like to include a number");
@@ -41,26 +43,23 @@ var confirmSpcChar = confirm ("Click Ok if you'd like to include a special chara
   }
 
 
-// concatenated the character outputs into the passwordOutput array
+// concatenated the random character into the passwordOutput array
 
-  if (confirmSpcChar) {
-    passwordOutput = passwordOutput.concat(specialChar)
+  if (confirmSpcChar === true) {
+    passwordOutput = passwordOutput.concat(specialChar);
   }
 
-  if (confirmNumber) {
-    passwordOutput = passwordOutput.concat(number)
+  if (confirmNumber === true) {
+    passwordOutput = passwordOutput.concat(number);
   }
     
-  if (confirmLowerCase) {
-    passwordOutput = passwordOutput.concat(lowerCase)
+  if (confirmLowerCase === true) {
+    passwordOutput = passwordOutput.concat(lowerCase);
   }
 
-  if (confirmUpperCase) {
-    passwordOutput = passwordOutput.concat(upperCase)
+  if (confirmUpperCase === true) {
+    passwordOutput = passwordOutput.concat(upperCase);
   }
-
-    console.log(passwordOutput)
-
 
   // empty password to be assigned based on the output from the for loop above
   var finalPassword = ""
@@ -68,14 +67,10 @@ var confirmSpcChar = confirm ("Click Ok if you'd like to include a special chara
   for (let i = 0; i < confirmLength; i++) {
 
     finalPassword = finalPassword + passwordOutput[Math.floor(Math.random() * passwordOutput.length)];
-    console.log(finalPassword)
+    console.log(finalPassword);
     }
     return finalPassword;
-
   }
-   
-
-
 
 // Write password to the #password input
 function writePassword() {
@@ -90,5 +85,4 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
 
